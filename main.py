258f16665f67cdd228802031c82054be9bbfb085
Witcher204 <puhtijenko@gmail.com>
2026-02-06 15:24:24 +0200

    Перше

---
+++ main.py
@@ -0,0 +1,18 @@
+x = 10
+name = "Yaroslav"
+is_student = True
+
+print(x)
+print(name)
+print(is_student)
+print("Hello, world")
+
+user = "Witcher204"
+color = "Red"
+is_epstein = True
+number = 31
+
+print(user)
+print(color)
+print(is_epstein)
+print(number)
